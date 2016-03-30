51c0035bad04a3674f7372bad422ae5ed16acb94
Ying-Ting Lin <claire4621@gmail.com>
2016-03-30 12:49:13 -0500

    Update MetropolisHastings.R

--- MetropolisHastings.R
+++ MetropolisHastings.R
@@ -20,19 +20,34 @@ GammaMHExample <- function(n.sim, n.burnin){
   theta.mh <- matrix(NA, nrow = n.sim, ncol = 2) #empty matrix to store sampled parameters
   theta.current <- rnorm(n = 2, mean = 3, sd = 0.5) #generate inital parameters
   theta.update <- function(index, theta.current,x,N) {
-    #sample a new parameter value from Gaussian
-    theta.star <- rnorm(n = 1, mean = theta.current[index], sd = 0.5)
-    if (index == 1) #if it's sample for alpha, update the first element of the parameter vector
+    if (index == 1){ #if it's sample for alpha, update the first element of the parameter vector
+      #sample a new parameter value from Gaussian
+      theta.star <- rnorm(n = 1, mean = theta.current[index], sd = 0.3)
+      #resample is drawn value is not positive
+      while (theta.star <=0){
+        print(theta.star);
+        theta.star <- rnorm(n = 1, mean = theta.current[index], sd = 0.3)
+        }
       theta.temp <- c(theta.star, theta.current[2])
-    else theta.temp <- c(theta.current[1], theta.star)#for beta, update the second element
-    #compute MH ratio
-    r <- post_den(theta.temp[1],theta.temp[2],x,N)/post_den(theta.current[1],theta.current[2],x,N)
-    r <- min(r, 1, na.rm = TRUE) # r is the accpetance probability
-    if (runif(1) < r)
+      #compute MH ratio
+     
+    }
+    else {
+      theta.star <- rnorm(n = 1, mean = theta.current[index], sd = 0.6)
+      #resample is drawn value is not positive
+      while (theta.star <=0){
+        print(theta.star);
+        theta.star <- rnorm(n = 1, mean = theta.current[index] , sd = 0.6);
+        }
+      theta.temp <- c(theta.current[1], theta.star)#for beta, update the second element
+      }
+      r <- post_den(theta.temp[1],theta.temp[2],x,N)/post_den(theta.current[1],theta.current[2],x,N)
+      r <- min(r, 1, na.rm = TRUE) # r is the accpetance probability, NA values are ignored
+      if (runif(1) < r)
       #runif(1) generates a uniform random number bewteen 0 and 1
       #if runif(1) < r, accept the new value; else reject the new value and keep the old value
-      theta.star
-    else theta.current[index]
+        theta.star
+      else theta.current[index]
   }
     for (i in 1:n.sim) {
     theta.current[1] <- theta.mh[i, 1] <- theta.update(1, theta.current,x, N) #iteration for alpha
@@ -59,10 +74,17 @@ plot(mh.draws)
 autocorr.plot(mh.draws)
 
 #Gelman and Rubin
+mh.draws1 <- mcmc(GammaMHExample(n.sim = 10000, n.burnin = 1000))
+mh.draws2 <- mcmc(GammaMHExample(n.sim = 10000, n.burnin = 1000))
+mh.draws3 <- mcmc(GammaMHExample(n.sim = 10000, n.burnin = 1000))
+mh.draws4 <- mcmc(GammaMHExample(n.sim = 10000, n.burnin = 1000))
+mh.draws5 <- mcmc(GammaMHExample(n.sim = 10000, n.burnin = 1000))
+mh.list <- mcmc.list(list(mh.draws1, mh.draws2, mh.draws3,mh.draws4, mh.draws5))
+gelman.diag(mh.list)  
+gelman.plot(mh.list)
 
 #Geweke 
 geweke.diag(mh.draws)
-
 
 #Raftery and Lewis
 raftery.diag(mh.draws,q=0.025,r=0.005,s=0.95)
